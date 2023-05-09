0c784ca828eeeeb7fed124c04da4d3e93362cd39
andybeet <22455149+andybeet@users.noreply.github.com>
2023-05-09 13:50:25 -0400

    update gitleaks workflow

--- runModel.sh
+++ runModel.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-RUN chmod -R 777 /HYDRA/mount
+chmod -R 777 /HYDRA/mount
 find /HYDRA/mount -type f | xargs dos2unix
-./hydra_sim -ind /HYDRA/mount/hydra_sim.dat -ainp /HYDRA/mount/hydra_sim.pin
+/HYDRA/hydra_sim -ind /HYDRA/mount/hydra_sim.dat -ainp /HYDRA/mount/hydra_sim.pin
